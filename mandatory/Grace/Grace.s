d651095f4e93bda993414bc04342e4e61baf7047
Ilan Nowak--Glandier <inowak--@1B5.42angouleme.fr>
2025-04-04 19:43:58 +0200

    grace

--- mandatory/Grace/Grace.s
+++ mandatory/Grace/Grace.s
@@ -1,16 +1,42 @@
-extern printf
 global main
-%macro grace
-fill
-%endmacro
-%macro fill_args 3
-mov rdx, %1
-mov rcx, %2
-mov r8, %3
-%endmacro
-%macro open
-fopen
-%endmacro
-grace
 section .data
-kid: "Grace_kid.s"
+filename db "Grace_kid.s", 0
+mode db "w", 0
+code db "%1$c%3$s%1$c%2$c", 0
+section .bss
+file_ptr resq 1
+section .text
+extern fopen, fprintf, fclose
+%macro Mopen 2
+mov rdi, %1
+mov rsi, %2
+call fopen
+test rax, rax
+jz error
+mov [file_ptr], rax
+%endmacro
+%macro Mprint 1
+mov rdi, [file_ptr]
+mov rsi, %1
+mov rdx, 34
+mov rcx, 10
+call fprintf
+%endmacro
+%macro Mclose 0
+mov rdi, [file_ptr]
+call fclose
+%endmacro
+main:
+push rbx
+Mopen filename, mode
+Mprint code
+Mclose
+xor rax, rax
+syscall
+jmp exit
+error:
+xor rax, rax
+syscall
+exit:
+pop rbx
+ret
--- mandatory/test_grace/Grace.s
+++ mandatory/test_grace/Grace.s
@@ -1,19 +1,42 @@
+global main
 section .data
-filename db "test.txt", 0   ; Nom du fichier (terminé par NULL)
-flags equ 0x42              ; O_CREAT | O_WRONLY (écriture, création si absent)
-mode equ 0o644              ; Permissions (rw-r--r--)
-
+filename db "Grace_kid.s", 0
+mode db "w", 0
+code db "%1$c%3$s%1$c%2$c", 0
+section .bss
+file_ptr resq 1
 section .text
-global _start
-
-_start:
-    mov rax, 2          ; Syscall `open`
-    mov rdi, filename   ; Nom du fichier
-    mov rsi, flags      ; Flags (O_CREAT | O_WRONLY)
-    mov rdx, mode       ; Mode (644)
-    syscall
-
-    mov rdi, rax        ; Sauvegarde du file descriptor
-    mov rax, 60         ; Syscall `exit`
-    xor rdi, rdi        ; Code de sortie 0
-    syscall
+extern fopen, fprintf, fclose
+%macro Mopen 2
+mov rdi, %1
+mov rsi, %2
+call fopen
+test rax, rax
+jz error
+mov [file_ptr], rax
+%endmacro
+%macro Mprint 1
+mov rdi, [file_ptr]
+mov rsi, %1
+mov rdx, 34
+mov rcx, 10
+call fprintf
+%endmacro
+%macro Mclose 0
+mov rdi, [file_ptr]
+call fclose
+%endmacro
+main:
+push rbx
+Mopen filename, mode
+Mprint code
+Mclose
+xor rax, rax
+syscall
+jmp exit
+error:
+xor rax, rax
+syscall
+exit:
+pop rbx
+ret
